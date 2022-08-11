bd1938b463a3940af361f57724321af079408122
mjca <matthewjakealix@gmail.com>
2022-08-11 22:20:03 +1000

    Add class `ConfigManager`

---
+++ learn2discover/configs/config_manager.py
@@ -0,0 +1,39 @@
+import os
+import yaml
+
+
+class ConfigManager:
+    config_file = 'config.yml'
+    instance = None
+    data_filepath = ''
+
+    def __init__(self, workspace_dir):
+        self.workspace_dir = workspace_dir
+
+    def get_yaml_configs(self):
+        yaml_path = os.path.join(self.workspace_dir, self.config_file)
+        print(yaml_path)
+        configs = {}
+        if os.path.exists(yaml_path):
+            stream = open(yaml_path, "r")
+            configs = yaml.full_load(stream)
+        return configs
+
+    @staticmethod
+    def get_instance():
+        if ConfigManager.instance is not None:
+            return ConfigManager.instance
+        else:
+            print('Error!')
+
+    @staticmethod
+    def create_instance(workspace_dir):
+        if ConfigManager.instance is None:
+            ConfigManager.instance = ConfigManager(workspace_dir)
+
+        return ConfigManager.instance
+
+    def load_configs(self):
+        configs = self.get_yaml_configs()
+        print('configs',configs)
+        self.data_filepath = configs.get('sut_settings').get('data_filepath')
--- learn2discover/l2d.py
+++ learn2discover/l2d.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from configs.configs_manager import ConfigManager
 
 current_path = os.path.dirname(os.path.realpath(__file__)) 
 try:
@@ -19,6 +20,8 @@ class Learn2Discover:
 
     def __init__(self):
         pass
+        self.config_manager = ConfigManager.create_instance('.')
+        self.config_manager.load_configs()
     def run(self):
         pass
 

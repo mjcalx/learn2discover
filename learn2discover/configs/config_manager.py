705ae82bf0796eb6857dd4f7d7283d06be74e99e
mjca <matthewjakealix@gmail.com>
2022-09-13 16:46:25 +1000

    Load schema file from config

--- learn2discover/configs/config_manager.py
+++ learn2discover/configs/config_manager.py
@@ -19,9 +19,9 @@ class ConfigManager:
     def __init__(self, workspace_dir):
         self.workspace_dir = workspace_dir
 
-    def get_yaml_configs(self):
-        yaml_path = os.path.join(self.workspace_dir, self.config_file)
-        print(yaml_path)
+    def get_yaml_configs(self, config_file=None):
+        cfg = config_file if config_file is not None else self.config_file
+        yaml_path = os.path.join(self.workspace_dir, cfg)
         configs = {}
         if os.path.exists(yaml_path):
             stream = open(yaml_path, "r")
@@ -42,10 +42,13 @@ class ConfigManager:
 
         return ConfigManager.instance
 
+    def get_data_schema(self):
+        return self.get_yaml_configs(self.schema_file)
+
     def load_configs(self):
         configs = self.get_yaml_configs()
-        print('configs',configs)
         self.model_path = configs.get('sut_settings').get('model_path')
+        self.schema_file = configs.get('data_settings').get('schema_file')
         self.training_path = configs.get('data_settings').get('training_path')
         self.validation_path = configs.get('data_settings').get('validation_path')
         self.evaluation_path = configs.get('data_settings').get('evaluation_path')

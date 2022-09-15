81c3c800b91d0c72a1fce7b19c5fb0d4501cf137
mjca <matthewjakealix@gmail.com>
2022-09-15 12:14:12 +1000

    Refactor `Schema` to subclass `OrderedDict`

--- learn2discover/data/schema.py
+++ learn2discover/data/schema.py
@@ -1,6 +1,7 @@
 from loggers.logger_factory import LoggerFactory
+from collections import OrderedDict
 
-class Schema:
+class Schema(OrderedDict):
     DESCRIPTION_STR = 'description'
     TYPE_STR        = 'type'
     CATEGORICAL_STR = 'categorical'
@@ -8,26 +9,20 @@ class Schema:
     VALUES_STR      = 'values'
 
     def __init__(self, parsed_yaml: dict):
-        self.schema = parsed_yaml
+        super(Schema, self).__init__(parsed_yaml)
         self.logger = LoggerFactory.get_logger(__class__.__name__) 
         self._validate()
-
-    def __getitem__(self, key):
-        return self.schema[key]
-
-    def keys(self):
-        return self.schema.keys()
     
     def _validate(self):
         """Check that categorical values are defined"""
         try:
-            for k in self.schema.keys():
-                var = self.schema[k]
+            for k in self.keys():
+                var = self[k]
                 var_type = var[self.TYPE_STR]
                 if var_type == self.CATEGORICAL_STR:
-                    assert self.VALUES_STR in self.schema[k].keys(), 'MissingValuesKey'
-                    assert isinstance(self.schema[k][self.VALUES_STR], dict) and \
-                            len(self.schema[k][self.VALUES_STR]) > 0, 'EmptyValuesDict'
+                    assert self.VALUES_STR in self[k].keys(), 'MissingValuesKey'
+                    assert isinstance(self[k][self.VALUES_STR], dict) and \
+                            len(self[k][self.VALUES_STR]) > 0, 'EmptyValuesDict'
                 elif var_type == self.NUMERICAL_STR:
                     pass
                 else:
@@ -46,8 +41,8 @@ class Schema:
     def get_description(self, variable_name: str):
         """Return description, if given in the schema. Else return the variable name."""
         try:
-            if self.DESCRIPTION_STR in self.schema[variable_name].keys():
-                return self.schema[variable_name][self.DESCRIPTION_STR]
+            if self.DESCRIPTION_STR in self[variable_name].keys():
+                return self[variable_name][self.DESCRIPTION_STR]
             return variable_name
         except KeyError:
             self.logger.error(f"KeyError: {variable_name}")

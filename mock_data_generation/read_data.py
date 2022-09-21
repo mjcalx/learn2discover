4cdcfdc358a254522a459f90871a929f9a2dcc22
mjca <matthewjakealix@gmail.com>
2022-09-21 16:27:21 +1000

    Remove logic duplicated in `Loader` class

--- mock_data_generation/read_data.py
+++ mock_data_generation/read_data.py
@@ -3,20 +3,6 @@ from typing import List
 
 from data.data_classes import DataAttributes, DataInstance, FileData
 
-
-def _headers_are_valid(headers: List[str], input_attributes: List[str], output_attributes: List[str]) -> bool:
-    """
-    Checks whether the headers received match the hardcoded expected attributes
-    """
-    input_count = len(input_attributes)
-
-    for i in range(len(headers)):
-        if (i < input_count and headers[i] != input_attributes[i]) or (
-                i >= input_count and headers[i] != output_attributes[i - input_count]):
-            return False
-    return True
-
-
 def _parse_instances(row_data: List[List[str]], attributes: DataAttributes) -> List[DataInstance]:
     """
     Parses raw row data to a list of DataInstances
@@ -39,7 +25,7 @@ def _parse_instances(row_data: List[List[str]], attributes: DataAttributes) -> L
     return instances
 
 
-def parse_data(filepath: str, input_attributes: List[str], output_attributes: List[str]) -> FileData:
+def parse_data(input_attributes: List[str], output_attributes: List[str]) -> FileData:
     """Reads a csv data file and parses it into relevant data types for model to use.
 
     Parameters:
@@ -52,23 +38,6 @@ def parse_data(filepath: str, input_attributes: List[str], output_attributes: Li
     Returns:
         A FileData object containing information on the file's attributes and data instances
     """
-    headers: List[str]
-    rows = []
-
-    with open(filepath, "r") as csv_file:
-        csv_reader = csv.reader(csv_file)
-
-        headers = next(csv_reader)
-
-        for row in csv_reader:
-            rows.append(row)
-
-    # Check that the headers are as expected
-    assert _headers_are_valid(
-        headers, input_attributes, output_attributes), f"Headers are:\n{headers}\nbut expected:\n\tinputs:" \
-                                                       f" {input_attributes}\n\toutputs:" \
-                                                       f" {output_attributes}"
-
     attributes = DataAttributes(input_attributes, output_attributes)
     instances = _parse_instances(rows, attributes)
 

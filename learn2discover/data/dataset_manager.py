4254515984985ddd312f718269dc4cd680ecee98
mjca <matthewjakealix@gmail.com>
2022-09-26 15:25:08 +1000

    Allow defined `DataAttributes` on `DatasetManager` init

--- learn2discover/data/dataset_manager.py
+++ learn2discover/data/dataset_manager.py
@@ -10,7 +10,7 @@ import pandas as pd
 class DatasetManager:
     instance = None
 
-    def __init__(self, random_state: int=42):
+    def __init__(self, random_state: int=42, attributes: DataAttributes=None):
         """_summary_
         Args:
             random_state (int, optional): random seed. Defaults to 42.
@@ -20,9 +20,12 @@ class DatasetManager:
         self.loader = Loader()
         self.schema, self.data = self.loader.load_data()
 
-        self.attributes = None
+        self.attributes = attributes
         self.X = None
         self.Y = None
+        if self.attributes is not None:
+            self.parse_data_instances()
+        
         self.outcomes = pd.Series([None]*len(self.data))
         self.fairness_labels = pd.Series([None]*len(self.data))
         DatasetManager.instance = self
@@ -33,7 +36,10 @@ class DatasetManager:
             DatasetManager.instance = DatasetManager()
         return DatasetManager.instance
 
-    def parse_data_instances(self, attributes: DataAttributes) -> None:
-        self.attributes = attributes
+    def parse_data_instances(self, attributes: DataAttributes=None) -> None:
+        if self.attributes is None and attributes is None:
+            raise ValueError("Unset DataManager.attributes")
+        if attributes is not None:
+            self.attributes = attributes
         self.X = self.data[self.attributes.inputs]
         self.Y = self.data[self.attributes.outputs]

020dad6913e7ef45aa523c742be79e47e3af3650
mjca <matthewjakealix@gmail.com>
2022-09-26 16:09:09 +1000

    Encapsulate SUT abstraction

--- data_generation/compas/get_compas_data.py
+++ data_generation/compas/get_compas_data.py
@@ -17,15 +17,16 @@ from data.data_classes import DataAttributes, Outcome, Label
 from data.dataset_manager import DatasetManager
 from collections import OrderedDict
 from compas_oracle import CompasOracle
+from system_under_test import SystemUnderTest
 
-ORACLE_TYPE = CompasOracle
-
-def _determine_compas_outcomes(outputs: pd.DataFrame) -> pd.Series:
-    """
-    Determines the outcome for each data instance based on its output values
-    """
-    assign_outcome = lambda x : Outcome.FAIL if str(x["ScoreText"]) in ["High", "Medium", "nan"] else Outcome.PASS
-    return outputs.apply(assign_outcome, axis=1)
+class Compas(SystemUnderTest):
+    def evaluate_outcomes(self, outputs: pd.DataFrame) -> pd.Series:
+        """
+        Determines the outcome for each data instance based on its output values
+        """
+        self.logger.debug("Evaluating SUT outcomes...")
+        assign_outcome = lambda x : Outcome.FAIL if str(x["ScoreText"]) in ["High", "Medium", "nan"] else Outcome.PASS
+        return outputs.apply(assign_outcome, axis=1)
 
 if __name__ == "__main__":
     """
@@ -43,11 +44,13 @@ if __name__ == "__main__":
                         'IsDeleted']
     INPUT_ATTRIBUTES = [i for i in schema.keys() if i not in set(OUTPUT_ATTRIBUTES)]
     SENSITIVE = ["Sex_Code_Text", "Ethnic_Code_Text"]
-    oracle = ORACLE_TYPE(SENSITIVE)
+    
+    sut    = Compas()
+    oracle = CompasOracle(SENSITIVE)
 
     attributes = DataAttributes(INPUT_ATTRIBUTES, OUTPUT_ATTRIBUTES)
     datamgr.parse_data_instances(attributes)
-    datamgr.outcomes = _determine_compas_outcomes(datamgr.Y)
+    datamgr.outcomes = sut.evaluate_outcomes(datamgr.Y)
 
     assert len(datamgr.outcomes) == len(datamgr.X)
     datamgr.fairness_labels = oracle.set_labels()

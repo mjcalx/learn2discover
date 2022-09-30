4b8e2ff7686034d14c7248f041e129d24d26263a
mjca <matthewjakealix@gmail.com>
2022-09-30 16:24:21 +1000

    Update `FTTensorDataset`
    - change type of `_tensors` property
    - add/update accessors for dict of tensors
    - add embedding size calculation

--- learn2discover/data/ft_tensor_dataset.py
+++ learn2discover/data/ft_tensor_dataset.py
@@ -13,30 +13,41 @@ class FTTensorDataset:
         self.logger = LoggerFactory.get_logger(__class__.__name__)
         self.schema = schema
         self._ftdata = ftdata
-        _vardict = lambda : {v:None for v in [VarType.CATEGORICAL, VarType.NUMERICAL]}
-        self._tensors = {pt:_vardict() for pt in ParamType}  # Dict of VarTypes per ParamType
+        # _vardict = lambda : {v:None for v in [VarType.CATEGORICAL, VarType.NUMERICAL]}
+        # self._tensors = {pt:_vardict() for pt in ParamType}  # Dict of VarTypes per ParamType
+        self._tensors = {v:None for v in [VarType.CATEGORICAL, VarType.NUMERICAL]}
         self._make_tensors()
 
+        cat_cols = self._tensors[VarType.CATEGORICAL][0]
+        col_size = lambda col : len(self._ftdata.flat_index()[col].cat.categories)
+        self.categorical_column_sizes = [col_size(col) for col in cat_cols]
+        self.logger.debug(f'Column value sizes: {list(zip(cat_cols,self.categorical_column_sizes))}')
+        self.categorical_embedding_sizes = [(col_size, min(50, (col_size+1)//2)) for col_size in self.categorical_column_sizes]
+        self.logger.debug(f'Categorical embedding sizes: {self.categorical_embedding_sizes}')
+
     def loc(self, idxs: pd.Index) -> Tuple[torch.Tensor, torch.Tensor]:
-        _get_categorical = lambda idxs : self._tensors[ParamType.INPUTS][VarType.CATEGORICAL][idxs]
-        _get_numerical = lambda idxs : self._tensors[ParamType.INPUTS][VarType.NUMERICAL][idxs]
+        _get_categorical = lambda idxs : self._tensors[VarType.CATEGORICAL][idxs]
+        _get_numerical = lambda idxs : self._tensors[VarType.NUMERICAL][idxs]
         tensors = (_get_categorical(idxs), _get_numerical(idxs))
         msg = "tensors of size {} (categorical) and {} (numerical) retrieved"
         self.logger.debug(msg.format(tensors[0].size(), tensors[1].size()))
         return tensors
+    
+    def tensors(self, vartype: VarType) -> Tuple[pd.Index, torch.Tensor]:
+        return self._tensors[vartype]
 
     def _make_categorical_tensors(self) -> None:
         """_summary_
         Apply a tensor transformation on the categorical input data and store a
         reference in self._tensors
         """
-        categorical_inputs = self._get_subframe_of_type(VarType.CATEGORICAL)
+        df_categorical = self._get_subframe_of_type(VarType.CATEGORICAL)
 
-        category_codes = [categorical_inputs[i].cat.codes.values for i in categorical_inputs.columns]
+        category_codes = [df_categorical[i].cat.codes.values for i in df_categorical.columns]
         categorical_data = torch.tensor(np.stack(category_codes, 1), dtype=torch.int64)
-        self._tensors[ParamType.INPUTS][VarType.CATEGORICAL] = categorical_data
+        self._tensors[VarType.CATEGORICAL] = (df_categorical.columns, categorical_data)
 
-        self.logger.debug(f'tensor data shape: {self._tensors[ParamType.INPUTS][VarType.CATEGORICAL].shape}')
+        self.logger.debug(f'tensor data shape: {self._tensors[VarType.CATEGORICAL][1].size()}')
 
     def _make_numerical_tensors(self) -> None:
         """_summary_
@@ -46,16 +57,16 @@ class FTTensorDataset:
         # Get subframe of columns of type DATE and convert to ints
         _date_inputs_subframe = self._get_subframe_of_type(VarType.DATE)
         str_to_int = lambda series : pd.to_datetime(series, infer_datetime_format=True).astype(int)
-        date_inputs = pd.DataFrame({col:str_to_int(_date_inputs_subframe[col]) for col in _date_inputs_subframe.columns})
+        df_date = pd.DataFrame({col:str_to_int(_date_inputs_subframe[col]) for col in _date_inputs_subframe.columns})
 
-        numerical_inputs = self._get_subframe_of_type(VarType.NUMERICAL)
+        df_numerical = self._get_subframe_of_type(VarType.NUMERICAL)
 
-        _combined_subframes = pd.concat([numerical_inputs, date_inputs], axis=1)
+        _combined_subframes = pd.concat([df_numerical, df_date], axis=1)
         _np_data = np.stack([_combined_subframes[col].values for col in _combined_subframes.columns], 1)
         numerical_data = torch.tensor(_np_data, dtype=torch.float)
-        self._tensors[ParamType.INPUTS][VarType.NUMERICAL] = numerical_data
+        self._tensors[VarType.NUMERICAL] = (_combined_subframes.columns, numerical_data)
 
-        self.logger.debug(f'numerical tensor data shape: {self._tensors[ParamType.INPUTS][VarType.NUMERICAL].shape}')
+        self.logger.debug(f'numerical tensor data shape: {self._tensors[VarType.NUMERICAL][1].size()}')
 
     def _make_tensors(self) -> None:
         self._make_categorical_tensors()
@@ -63,6 +74,7 @@ class FTTensorDataset:
 
     def _get_subframe_of_type(self, vartype: VarType) -> pd.DataFrame:
         _var_cols = self.schema.vars_by_type(vartype)
-        input_idxs = [col for col in _var_cols if col in self._ftdata.attributes.inputs]
+        # input_idxs = [col for col in _var_cols if col in self._ftdata.attributes.inputs]
+        input_idxs = _var_cols
         subframe = self._ftdata.attribute_data[input_idxs]
         return subframe

4030bb55ca0259aff2186ef22feb2ca855b83111
abigail-rose <abigail.rose@me.com>
2022-10-17 15:56:04 +1100

    Update preprocess_compas to include header

--- datasets/original/COMPAS/preprocess_compas.py
+++ datasets/original/COMPAS/preprocess_compas.py
@@ -6,14 +6,23 @@ import csv
 
 SCORE_TEXT_ROW = 24
 
-lines = list()
-with open('compas-scores-raw.csv', 'r') as readFile:
-    reader = csv.reader(readFile)
-    for row in reader:
-        # Remove invalid rows
-        if row[24] in ["High", "Medium", "Low"]: 
-            lines.append(row)
-with open('compas-scores-preprocessed.csv', 'w') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(lines)
+def preprocess_compas():
+    lines = list()
+    with open('compas-scores-raw.csv', 'r') as readFile:
+        reader = csv.reader(readFile)
 
+        # Add header to new file
+        header = next(reader)
+        lines.append(header)
+
+        for row in reader:
+            # Remove invalid rows
+            if row[SCORE_TEXT_ROW] in ["High", "Medium", "Low"]: 
+                lines.append(row)
+        
+    with open('compas-scores-preprocessed.csv', 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(lines)
+
+if __name__ == "__main__":
+    preprocess_compas()

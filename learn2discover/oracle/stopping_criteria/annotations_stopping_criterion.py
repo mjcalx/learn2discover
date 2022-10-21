115de0ea0699f76c7782258156f28cbad7897740
mjca <matthewjakealix@gmail.com>
2022-10-21 17:25:57 +1100

    Assert unlabelled data length per `StoppingCriterion`

--- learn2discover/oracle/stopping_criteria/annotations_stopping_criterion.py
+++ learn2discover/oracle/stopping_criteria/annotations_stopping_criterion.py
@@ -1,11 +1,19 @@
+from configs.config_manager import ConfigManager
 from data.dataset_manager import DatasetManager
 from oracle.stopping_criteria.stopping_criterion import StoppingCriterion
+
 
 class AnnotationsStoppingCriterion(StoppingCriterion):
     def __init__(self):
         super(AnnotationsStoppingCriterion, self).__init__()
         self.dmgr = DatasetManager.get_instance()
         self.num_annotations = self.settings['num_annotations']
+
+        _unlab_len = len(self.dmgr.data.unlabelled_data)
+        if _unlab_len < self.num_annotations:
+            _m = 'Not enough unlabelled data for stopping criterion. Needed >={}, received {}.'
+            self.logger.error(_m.format(self.num_annotations, _unlab_len))
+            raise AssertionError
 
     @property
     def name(self):
--- learn2discover/oracle/stopping_criteria/iterations_stopping_criterion.py
+++ learn2discover/oracle/stopping_criteria/iterations_stopping_criterion.py
@@ -1,10 +1,20 @@
+from configs.config_manager import ConfigManager
+from data.dataset_manager import DatasetManager
 from oracle.stopping_criteria.stopping_criterion import StoppingCriterion
+
 
 class IterationsStoppingCriterion(StoppingCriterion):
     def __init__(self):
         super(IterationsStoppingCriterion, self).__init__()
         self.max_iterations = self.settings['num_iterations']
         self.iterations = 0
+
+        _min_len = self.max_iterations * ConfigManager.get_instance().unlabelled_sampling_size
+        _unlab_len = len(DatasetManager.get_instance().data.unlabelled_data)
+        if _min_len > _unlab_len:
+            _m = 'Not enough unlabelled data for stopping criterion. Needed >={}, received {}.'
+            self.logger.error(_m.format(_min_len, _unlab_len))
+            raise AssertionError
 
     @property
     def name(self):
